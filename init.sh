c4216782a3f0764038727caf4e55dff36ac040e8
EC2 Default User <ec2-user@ip-172-26-10-85.ap-northeast-2.compute.internal>
2020-12-24 14:49:20 +0000

    Add docker and docker-compose installation

--- init.sh
+++ init.sh
@@ -8,3 +8,12 @@ curl -fsSL https://code-server.dev/install.sh | sh
 cp -f ./code-server-config.yaml $CODE_SERVER_CONFIG_PATH
 sed -i "s|\$CODE_SERVER_PASSWORD|$password|g" $CODE_SERVER_CONFIG_PATH
 sudo systemctl enable --now code-server@$USER
+
+####
+
+sudo yum update -y
+sudo yum install docker 
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
